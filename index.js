133b5e5da692d03619eccc2db82ae4211ffa0873
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-08-10 07:19:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, holiday) {
+    const array = []
+    for (let i = 0; i < names.length; i++) {
+      let card = (`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+      array.push(card)
+    }
+    return array
+};
+
+function countDown(number) {
+    let starting_number = number
+    while (starting_number >= 0) { console.log(starting_number--) }
+};
